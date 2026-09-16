14211d1b4215d4358f59303b59d4b946ceceeb18
pabloernestomartinez-collab <pabloernestomartinez@upc.edu.ar>
2026-09-15 22:33:00 -0300

    menu pulperia

---
+++ Assets/Scripts/game/coleccionables/menuInventario.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+[CreateAssetMenu (fileName = "objeto",menuName = "objeto pulperia")]
+public class menuInventario : ScriptableObject
+{
+    public Sprite imagenObjeto;
+    public string nombreObjeto;
+}
